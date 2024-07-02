35d0cc4112775121ad0d0a6fc4dc3a267da1812d
rishita <rishitagupta2700@gmail.com>
2024-07-02 15:28:59 -0400

    Initial Layout

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,17 @@
+import { Typography } from 'antd'
+import React from 'react'
+
+const {Title} = Typography;
+
+const Homepage = () => {
+  return (
+    <>
+    {/* <Title level={2} className='heading'> Global Crypto Stats</ Title>
+    <Row>
+      <Col span ={12}></Col>
+    </Row> */}
+    </>
+  )
+}
+
+export default Homepage
